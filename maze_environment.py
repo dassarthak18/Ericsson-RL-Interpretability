7d3b75a485f0154d0f57491ac5c4962b0fcc87d8
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2021-12-13 06:34:37 +0000

    Update maze_environment.py

--- maze_environment.py
+++ maze_environment.py
@@ -1,6 +1,21 @@
 import gym
 from gym import spaces
 
+MAP = [ ['A', 'A', 'A', 'A', 'A', '*', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'E', 'E', 'E', 'E', 'E'],
+        ['A', 'A', 'A', 'A', 'A', 'A', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'E', 'E', 'E', 'E', 'E'],
+        ['A', 'A', 'A', 'A', 'A', 'A', 'B', '*', 'B', 'B', 'B', 'B', 'B', 'E', 'E', 'E', 'E', 'E'],
+        ['A', 'A', 'A', 'A', 'o', 'A', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'E', 'E', '*', 'E', 'E'],
+        ['a', 'a', 'a', 'a', 'o', 'a', 'b', 'b', 'b', 'B', 'B', 'B', 'B', 'E', 'E', 'E', 'E', 'E'],
+        ['a', 'a', 'a', 'a', 'S', 'o', 'o', 'b', 'o', 'o', 'B', 'B', 'B', 'E', 'E', 'E', 'E', 'E'],
+        ['a', 'o', 'a', 'a', 'a', 'a', 'b', 'b', 'b', 'B', 'B', 'B', 'B', 'E', 'E', 'E', 'E', 'E'],
+        ['D', 'o', 'D', 'D', 'D', 'D', 'b', 'b', 'b', 'B', 'B', 'B', 'B', 'E', 'E', 'E', 'E', 'E'],
+        ['D', 'D', 'D', '*', 'D', 'D', 'b', 'o', 'b', 'B', 'B', 'B', 'B', 'E', 'E', 'E', 'E', 'E'],
+        ['D', 'D', 'D', 'D', 'D', 'D', 'C', 'o', 'C', 'C', 'C', 'C', 'C', 'E', 'E', 'E', 'E', 'E'],
+        ['D', 'D', 'D', 'D', 'D', 'D', 'C', 'C', 'C', 'C', 'C', '*', 'C', 'E', 'E', 'E', 'E', 'E'],
+        ['D', 'G', 'D', 'D', 'D', 'D', 'C', 'C', 'C', 'C', 'C', 'C', 'o', 'o', 'E', 'E', 'E', 'E'],
+        ['D', 'D', 'D', 'D', 'D', 'D', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'E', 'E', 'E', 'E', '*']]
+       
+
 class MazeEnv(gym.Env):
   
   """
@@ -9,7 +24,7 @@ class MazeEnv(gym.Env):
   A A A A A A B * B B B B B E E E E E
   A A A A o A B B B B B B B E E * E E
   a a a a o a b b b B B B B E E E E E
-  a a a a S o o b b o o B B E E E E E
+  a a a a S o o b o o B B B E E E E E
   a o a a a a b b b B B B B E E E E E
   D o D D D D b b b B B B B E E E E E
   D D D * D D b o b B B B B E E E E E
@@ -17,7 +32,7 @@ class MazeEnv(gym.Env):
   D D D D D D C C C C C * C E E E E E
   D G D D D D C C C C C C o o E E E E
   D D D D D D C C C C C C C E E E E *
-
+  
   A - Room A, a - Hall A, B - Room B,
   b - Hall B, C - Room C, D - Room D,
   E - Room E, * - Flag, S - Start State,
