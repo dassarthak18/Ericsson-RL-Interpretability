952beb7b6abf608fbb8d5a0aaf945a00a32696aa
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2021-12-14 05:39:14 +0000

    Update maze_environment.py

--- maze_environment.py
+++ maze_environment.py
@@ -2,41 +2,41 @@ import copy
 import gym
 from gym import spaces
 
-MAP = [['A', 'A', 'A', 'A', 'A', '*', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'E', 'E', 'E', 'E', 'E'],
-['A', 'A', 'A', 'A', 'A', 'A', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'E', 'E', 'E', 'E', 'E'],
-['A', 'A', 'A', 'A', 'A', 'A', 'B', '*', 'B', 'B', 'B', 'B', 'B', 'E', 'E', 'E', 'E', 'E'],
-['A', 'A', 'A', 'A', 'o', 'A', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'E', 'E', '*', 'E', 'E'],
-['a', 'a', 'a', 'a', 'o', 'a', 'b', 'b', 'b', 'B', 'B', 'B', 'B', 'E', 'E', 'E', 'E', 'E'],
-['a', 'a', 'a', 'a', 'S', 'o', 'o', 'b', 'o', 'o', 'B', 'B', 'B', 'E', 'E', 'E', 'E', 'E'],
-['a', 'o', 'a', 'a', 'a', 'a', 'b', 'b', 'b', 'B', 'B', 'B', 'B', 'E', 'E', 'E', 'E', 'E'],
-['D', 'o', 'D', 'D', 'D', 'D', 'b', 'b', 'b', 'B', 'B', 'B', 'B', 'E', 'E', 'E', 'E', 'E'],
-['D', 'D', 'D', '*', 'D', 'D', 'b', 'o', 'b', 'B', 'B', 'B', 'B', 'E', 'E', 'E', 'E', 'E'],
-['D', 'D', 'D', 'D', 'D', 'D', 'C', 'o', 'C', 'C', 'C', 'C', 'C', 'E', 'E', 'E', 'E', 'E'],
-['D', 'D', 'D', 'D', 'D', 'D', 'C', 'C', 'C', 'C', 'C', '*', 'C', 'E', 'E', 'E', 'E', 'E'],
-['D', 'G', 'D', 'D', 'D', 'D', 'C', 'C', 'C', 'C', 'C', 'C', 'o', 'o', 'E', 'E', 'E', 'E'],
-['D', 'D', 'D', 'D', 'D', 'D', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'E', 'E', 'E', 'E', '*']]
+MAP = [[1, 1, 1, 1, 1, -1, 2, 2, 2, 2, 2, 2, 2, 5, 5, 5, 5, 5],
+       [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 5, 5, 5, 5, 5],
+       [1, 1, 1, 1, 1, 1, 2, -1, 2, 2, 2, 2, 2, 5, 5, 5, 5, 5],
+       [1, 1, 1, 1, 9, 1, 2, 2, 2, 2, 2, 2, 2, 5, 5, -1, 5, 5],
+       [6, 6, 6, 6, 9, 6, 7, 7, 7, 2, 2, 2, 2, 5, 5, 5, 5, 5],
+       [6, 6, 6, 6, 0, 9, 9, 7, 9, 9, 2, 2, 2, 5, 5, 5, 5, 5],
+       [6, 9, 6, 6, 6, 6, 7, 7, 7, 2, 2, 2, 2, 5, 5, 5, 5, 5],
+       [4, 9, 4, 4, 4, 4, 7, 7, 7, 2, 2, 2, 2, 5, 5, 5, 5, 5],
+       [4, 4, 4, -1, 4, 4, 7, 9, 7, 2, 2, 2, 2, 5, 5, 5, 5, 5],
+       [4, 4, 4, 4, 4, 4, 3 , 9, 3 , 3 , 3 , 3 , 3 , 5, 5, 5, 5, 5],
+       [4, 4, 4, 4, 4, 4, 3 , 3 , 3 , 3 , 3 , -1, 3 , 5, 5, 5, 5, 5],
+       [4, 8, 4, 4, 4, 4, 3 , 3 , 3 , 3 , 3 , 3 , 9, -1, 5, 5, 5, 5],
+       [4, 4, 4, 4, 4, 4, 3 , 3 , 3 , 3 , 3 , 3 , 3 , 5, 5, 5, 5, -1]]
 
 class MazeEnv(gym.Env):
 
        """
-       A A A A A * B B B B B B B E E E E E
-       A A A A A A B B B B B B B E E E E E
-       A A A A A A B * B B B B B E E E E E
-       A A A A o A B B B B B B B E E * E E
-       a a a a o a b b b B B B B E E E E E
-       a a a a S o o b o o B B B E E E E E
-       a o a a a a b b b B B B B E E E E E
-       D o D D D D b b b B B B B E E E E E
-       D D D * D D b o b B B B B E E E E E
-       D D D D D D C o C C C C C E E E E E
-       D D D D D D C C C C C * C E E E E E
-       D G D D D D C C C C C C o o E E E E
-       D D D D D D C C C C C C C E E E E *
+       1 1 1 1 1 -1 2 2 2 2 2 2 2 5 5 5 5 5
+       1 1 1 1 1 1 2 2 2 2 2 2 2 5 5 5 5 5
+       1 1 1 1 1 1 2 -1 2 2 2 2 2 5 5 5 5 5
+       1 1 1 1 9 1 2 2 2 2 2 2 2 5 5 -1 5 5
+       6 6 6 6 9 6 7 7 7 2 2 2 2 5 5 5 5 5
+       6 6 6 6 0 9 9 7 9 9 2 2 2 5 5 5 5 5
+       6 9 6 6 6 6 7 7 7 2 2 2 2 5 5 5 5 5
+       4 9 4 4 4 4 7 7 7 2 2 2 2 5 5 5 5 5
+       4 4 4 -1 4 4 7 9 7 2 2 2 2 5 5 5 5 5
+       4 4 4 4 4 4 3 9 3 3 3 3 3 5 5 5 5 5
+       4 4 4 4 4 4 3 3 3 3 3 -1 3 5 5 5 5 5
+       4 8 4 4 4 4 3 3 3 3 3 3 9 9 5 5 5 5
+       4 4 4 4 4 4 3 3 3 3 3 3 3 5 5 5 5 -1
 
-       A - Room A, a - Hall A, B - Room B,
-       b - Hall B, C - Room C, D - Room D,
-       E - Room E, * - Flag, S - Start State,
-       G - Goal State, o - Door
+       1 - Room A, 6 - Hall A, 2 - Room B,
+       7 - Hall B, 3 - Room C, 4 - Room D,
+       5 - Room E, -1 - Flag, 0 - Start State,
+       8 - Goal State, 9 - Door
 
        Can move up, down, left or right within a room.
        Can move to another room using doors.
@@ -45,11 +45,11 @@ class MazeEnv(gym.Env):
        An episode ends when goal state is reached.
        
        Observation:
-              Type: Discrete(3)
-              Num Observation
-              0   x-coordinate
-              1   y-coordinate
-              2   location
+              Type: Box(3)
+              Num Observation   Min  Max
+              0   x-coordinate  0    17
+              1   y-coordinate  0    12
+              2   location      -1   9
 
        Actions:
               Type: Discrete(4)
