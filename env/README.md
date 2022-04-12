cfbfad42cdbb61643aa1c47f3a744a0101463e1a
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2022-04-12 08:43:51 +0000

    Add files via upload

---
+++ env/README.md
@@ -0,0 +1,1 @@
+This folder contains custom Gym environments for RL simulation and interpretability.
--- src/print.py
+++ src/print.py
@@ -17,7 +17,44 @@ def print_maze(env,model):
         start = 1
       if maze[i][j] == 3:
         goal = 1
-      maze[i][j] = ACT_DICT[str(output(model,(i,j,start,goal,flag)))]
+      # Info of surroundings
+      F_N = 0
+      S_N = 0
+      G_N = 0
+      F_S = 0
+      S_S = 0
+      G_S = 0
+      F_E = 0
+      S_E = 0
+      G_E = 0
+      F_W = 0
+      S_W = 0
+      G_W = 0
+      if i != 0 and env.maze[i-1][j] == 1:
+             F_N = 1
+      if i != 0 and env.maze[i-1][j] == 2:
+             S_N = 1
+      if i != 0 and env.maze[i-1][j] == 3:
+             G_N = 1
+      if i != len(maze)-1 and env.maze[i+1][j] == 1:
+             F_S = 1
+      if i != len(maze)-1 and env.maze[i+1][j] == 2:
+             S_S = 1
+      if i != len(maze)-1 and env.maze[i+1][j] == 3:
+             G_S = 1
+      if j != 0 and env.maze[i][j-1] == 1:
+             F_E = 1
+      if j != 0 and env.maze[i][j-1] == 2:
+             S_E = 1
+      if j != 0 and env.maze[i][j-1] == 3:
+             G_E = 1
+      if j != len(maze[0])-1 and env.maze[i][j+1] == 1:
+             F_W = 1
+      if j != len(maze[0])-1 and env.maze[i][j+1] == 2:
+             S_W = 1
+      if j != len(maze[0])-1 and env.maze[i][j+1] == 3:
+             G_W = 1
+      maze[i][j] = ACT_DICT[str(output(model,(S_N,G_N,F_N,S_S,G_S,F_S,S_E,G_E,F_E,S_W,G_W,F_W)))]
 
   for i in range(len(maze)):
     s = ''
@@ -35,7 +72,44 @@ def print_maze(env,model):
         start = 1
       if maze[i][j] == 3:
         goal = 1
-      maze[i][j] = ACT_DICT[str(output(model,(i,j,start,goal,0)))]
+      # Info of surroundings
+      F_N = 0
+      S_N = 0
+      G_N = 0
+      F_S = 0
+      S_S = 0
+      G_S = 0
+      F_E = 0
+      S_E = 0
+      G_E = 0
+      F_W = 0
+      S_W = 0
+      G_W = 0
+      if i != 0 and maze[i-1][j] == 1:
+             F_N = 1
+      if i != 0 and maze[i-1][j] == 2:
+             S_N = 1
+      if i != 0 and maze[i-1][j] == 3:
+             G_N = 1
+      if i != len(maze)-1 and env.maze[i+1][j] == 1:
+             F_S = 1
+      if i != len(maze)-1 and env.maze[i+1][j] == 2:
+             S_S = 1
+      if i != len(maze)-1 and env.maze[i+1][j] == 3:
+             G_S = 1
+      if j != 0 and env.maze[i][j-1] == 1:
+             F_E = 1
+      if j != 0 and env.maze[i][j-1] == 2:
+             S_E = 1
+      if j != 0 and env.maze[i][j-1] == 3:
+             G_E = 1
+      if j != len(maze[0])-1 and env.maze[i][j+1] == 1:
+             F_W = 1
+      if j != len(maze[0])-1 and env.maze[i][j+1] == 2:
+             S_W = 1
+      if j != len(maze[0])-1 and env.maze[i][j+1] == 3:
+             G_W = 1
+      maze[i][j] = ACT_DICT[str(output(model,(S_N,G_N,F_N,S_S,G_S,F_S,S_E,G_E,F_E,S_W,G_W,F_W)))]
 
   for i in range(len(maze)):
     s = ''
